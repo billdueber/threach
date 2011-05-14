d0f42d60e1e336ba88f63438af4c0c3809f3a4af
BillDueber <bill@dueber.com>
2011-05-14 09:34:59 -0400

    Added code to trap exceptions and breaks

--- lib/threach.rb
+++ lib/threach.rb
@@ -1,34 +1,73 @@
 require 'thread'
 module Enumerable
   
+  class ThreachDone < StandardError; end
+  
   def threach(threads=0, iterator=:each, &blk)
     if threads == 0
       self.send(iterator) do |*args|
         blk.call *args
       end
     else
-      bq = SizedQueue.new(threads * 2)
-      consumers = []
-      threads.times do |i|
-        consumers << Thread.new(i) do |i|
-          until (a = bq.pop) === :end_of_data
-            blk.call(*a)
-          end
-        end          
-      end
+      begin
+        bq = SizedQueue.new(threads * 2) # the work queue
+        dq = SizedQueue.new(threads)     # threads that are done
+        eq = SizedQueue.new(threads)     # errors
+      
+        consumers = []
+        threads.times do |i|
+          consumers << Thread.new(i) do |i|
+            Thread.current[:tnum] = i
+            begin
+              while true
+                a = bq.pop
+                if a  === :end_of_data
+                  Thread.current[:normalExit] = true
+                  break
+                end
+                blk.call(*a)
+              end
+            rescue LocalJumpError
+            rescue Exception => e
+              eq << e
+              Thread.current[:error] = e
+              bq.pop # make sure the producer can move on and check eq
+            ensure
+              dq << self
+              # If we didn't exit normally or due to an error, it was a break
+              unless Thread.current[:normalExit] or Thread.current[:error]
+                eq << ThreachDone.new
+              end
+            end
+          end          
+        end
     
-      # The producer
-      count = 0
-      self.send(iterator) do |*x|
-        bq.push x
-        count += 1
+        # The producer
+        count = 0
+        self.send(iterator) do |*x|
+          # Bail if we've got an error
+          if eq.size > 0
+            e  = eq.pop
+            raise e
+          end
+          bq.push x
+          count += 1
+        end
+        
+        # Now end it. 
+        Thread.exclusive do
+          bq.pop if bq.size > 0
+          threads.times do
+            bq << :end_of_data
+          end
+        end
+        # Do the join
+        consumers.each {|t| t.join}
+      rescue ThreachDone => e
+        # do nothing. We bailed out because of a break
+      rescue Exception => e
+        raise e
       end
-      # Now end it
-      threads.times do 
-        bq << :end_of_data
-      end
-      # Do the join
-      consumers.each {|t| t.join}
     end
   end
     
